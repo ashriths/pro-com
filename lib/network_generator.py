5e14546b259892943dfaa943570fff986eff962b
ashishrawat93 <puneashish@yahoo.co.in>
2015-02-27 10:18:27 +0530

    fnskdj
    sdfn

--- lib/network_generator.py
+++ lib/network_generator.py
@@ -12,21 +12,35 @@ class NetworkGenerator:
         self.listener = None
         logging.basicConfig(filename='server.log', filemode='w', level=logging.DEBUG)
         self.logger = logging.getLogger('serverLogger')
-        self.logger.setConsoleLevel(logging.INFO)
+        ##self.logger.setConsoleLevel(logging.INFO)
 
     @property
     def ip(self):
         return socket.gethostname()
 
+    def broadcast(self, msg):
+        for node in self.nodes:
+            self.socket.bind(node):
+            
+            print str(node)
+
     def listen(self):
         while True:
             connection, address = self.socket.accept()
             self.logger.info("New request to join Network (Address : %s )" % str(address))
-            self.nodes.append(connection)
+            self.nodes.append(address)
+            name = connection.recv(1024)
+            print "New request to join Network (name : %s , Address : %s )" % (name, str(address))
+            connection.send("The server has accepted you request. ")
+            self.broadcast("New Node in network called "+name)
+            self.socket.bind((self.ip, port))
+            self.socket.listen(5)
+
 
     def start(self, port=5000):
+        self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.ip, port))
+        self.socket.bind((self.ip, self.port))
         self.logger.info("Server created on %s listening to port %s" % (self.ip, port))
         self.socket.listen(5)
         self.listen()
--- util/tcp_client.py
+++ util/tcp_client.py
@@ -3,7 +3,7 @@ __author__ = 'admin'
 import socket
 
 ''' client '''
-
+import random
 TCP_IP = '127.0.0.1'
 
 TCP_PORT = 5000
@@ -12,9 +12,9 @@ MESSAGE = "Hello, World!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-
+s.send(str(random.randint(0, 10)))
 data = s.recv(BUFFER_SIZE)
+
 s.close()
 
 print "Server Reply:", data
