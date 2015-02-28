7fd91a526606ecdd273009c6d07dca260b3348fe
Ashrith Sheshan <ashrith.sheshan@nutanix.com>
2015-02-28 11:29:14 +0530

    Add Network status

--- lib/network_generator.py
+++ lib/network_generator.py
@@ -3,7 +3,6 @@ __author__ = 'ashrith'
 import socket
 import logging
 
-
 class NetworkGenerator:
     def __init__(self):
         self.nodes = []
@@ -12,17 +11,23 @@ class NetworkGenerator:
         self.listener = None
         logging.basicConfig(filename='server.log', filemode='w', level=logging.DEBUG)
         self.logger = logging.getLogger('serverLogger')
-        self.logger.setConsoleLevel(logging.INFO)
+        ## self.logger.setConsoleLevel(logging.INFO)
 
     @property
     def ip(self):
         return socket.gethostname()
+
 
     def listen(self):
         while True:
             connection, address = self.socket.accept()
             self.logger.info("New request to join Network (Address : %s )" % str(address))
             self.nodes.append(connection)
+            print "New request to join Network (Address : %s )" % str(address)
+            print "Total Nodes in Network = " + str(len(self.nodes))
+            print self.nodes
+
+
 
     def start(self, port=5000):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
